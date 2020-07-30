f4dc03e6cd021709f6504c05b552958dfa365fb8
ShawnHx <hx36w35@163.com>
2020-07-30 21:15:21 +0800

    Add self-defined streaming demo and modify the default value of taskmanager.numberOfTaskSlots

--- flink-runtime/src/main/java/org/apache/flink/runtime/jobmaster/slotpool/MySlotSelectionStrategy.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/jobmaster/slotpool/MySlotSelectionStrategy.java
@@ -1,31 +1,4 @@
 package org.apache.flink.runtime.jobmaster.slotpool;
 
-import org.apache.flink.runtime.clusterframework.types.ResourceProfile;
-import org.apache.flink.runtime.jobmanager.scheduler.Locality;
-
-import javax.annotation.Nonnull;
-import java.util.*;
-
-public class MySlotSelectionStrategy extends LocationPreferenceSlotSelectionStrategy {
-
-	@Nonnull
-	@Override
-	protected Optional<SlotInfoAndLocality> selectWithoutLocationPreference(@Nonnull Collection<SlotInfoAndResources> availableSlots, @Nonnull ResourceProfile resourceProfile) {
-		List<SlotInfoAndResources> candidates = new ArrayList<>();
-		for (SlotInfoAndResources candidate : availableSlots) {
-			if (candidate.getRemainingResources().isMatching(resourceProfile)) {
-				candidates.add(candidate);
-			}
-		}
-		if (!candidates.isEmpty()) {
-			SlotInfoAndResources last = candidates.get(candidates.size() - 1);
-			return Optional.of(SlotInfoAndLocality.of(last.getSlotInfo(), Locality.UNCONSTRAINED));
-		}
-		return Optional.empty();
-	}
-
-	@Override
-	protected double calculateCandidateScore(int localWeigh, int hostLocalWeigh, double taskExecutorUtilization) {
-		return localWeigh * 2 + hostLocalWeigh;
-	}
+public class MySlotSelectionStrategy {
 }
